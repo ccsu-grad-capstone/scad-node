96f47ca76537b645b2543ece0fcc59a77f65ca85
Ryan Lauzon <lauzon232@gmail.com>
2020-09-15 09:56:50 -0400

    update getTransactions to only return latest 50

--- services/yahooFantasy.js
+++ services/yahooFantasy.js
@@ -2,24 +2,18 @@ var YahooFantasy = require('yahoo-fantasy')
 const debug = require('debug')('app:yahooFantasy')
 const { YAHOO_CLIENT_SECRET, YAHOO_CLIENT_ID } = require('../config')
 
-const GAMEKEY = '399.l.'
+const GAMEKEY = '399'
 
 
 function yahooFantasy() {
 
-  function init(accessToken) {
-    var yf = new YahooFantasy(YAHOO_CLIENT_ID, YAHOO_CLIENT_SECRET)
-    yf.setUserToken(accessToken)
-    return yf
-  }
-
   async function getTransactions(accessToken, yahooLeagueId) {
-    
     try {
       var yf = new YahooFantasy(YAHOO_CLIENT_ID, YAHOO_CLIENT_SECRET)
       yf.setUserToken(accessToken)
       
-      let result = await yf.league.transactions(`399.l.13088`)
+      let result = await yf.league.transactions(`${GAMEKEY}.l.${yahooLeagueId}`)
+      result.transactions = result.transactions.slice(0, 50)
       return result
       
     } catch (error) {
