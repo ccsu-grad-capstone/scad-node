c86323d5515675584a958fd342feabb2d767baef
Ryan Lauzon <lauzon232@gmail.com>
2021-02-17 13:34:15 -0500

    updated yahoo service with getGame options

--- routes/api-v2-yahoo.js
+++ routes/api-v2-yahoo.js
@@ -5,13 +5,19 @@ const scadAuth = require('../utilities/scadAuth')
 
 const yahooRouter = express.Router()
 
+// Already in LAMBDAS
 yahooRouter.get('/myTeams/:accessToken', scadAuth(), getMyTeams)
 yahooRouter.get('/league/:yahooLeagueId/myTeam/:accessToken', scadAuth(), getMyTeam)
+yahooRouter.get('/league/:yahooLeagueId/:accessToken', scadAuth(), getLeagueMeta)
 yahooRouter.get('/league/:yahooLeagueId/settings/:accessToken', scadAuth(), getLeagueSettings)
 yahooRouter.get('/league/:yahooLeagueId/standings/:accessToken', scadAuth(), getLeagueStandings)
 yahooRouter.get('/league/:yahooLeagueId/teams/:accessToken', scadAuth(), getLeagueTeams)
 yahooRouter.get('/league/all/:accessToken', scadAuth(), getAllUsersLeagues)
 yahooRouter.get('/league/:yahooLeagueId/transactions/:accessToken', scadAuth(), getLeagueTransactions)
+yahooRouter.get('/league/:yahooLeagueId/team/:yahooTeamId/roster/:accessToken', scadAuth(), getRoster)
+
+// Need to add to LAMBDAS
+yahooRouter.get('/game/:accessToken', scadAuth(), getGames)
 
 module.exports = yahooRouter
 
@@ -31,13 +37,26 @@ async function getMyTeams(req, res) {
 async function getMyTeam(req, res) {
   const { accessToken, yahooLeagueId } = req.params
   try {
-    let result = await yf.getMyTeams(accessToken, yahooLeagueId)
+    let result = await yf.getMyTeam(accessToken, yahooLeagueId)
     res.json({
-      myTeam: result.teams[0].teams.find(t => t.team_key.includes(yahooLeagueId))
+      myTeam: result
     })
   } catch (error) {
     debug(error)
     res.status(500).send('An Error Occured Retrieving MyTeam')
+  }
+}
+
+async function getLeagueMeta(req, res) {
+  const { accessToken, yahooLeagueId } = req.params
+  try {
+    let result = await yf.getLeagueMeta(accessToken, yahooLeagueId)
+    res.json({
+      league: result,
+    })
+  } catch (error) {
+    debug(error)
+    res.status(500).send('An Error Occured Retrieving League Settings')
   }
 }
 
@@ -46,7 +65,7 @@ async function getLeagueSettings(req, res) {
   try {
     let result = await yf.getLeagueSettings(accessToken, yahooLeagueId)
     res.json({
-      settings: result,
+      settings: result.settings,
     })
   } catch (error) {
     debug(error)
@@ -84,7 +103,6 @@ async function getAllUsersLeagues(req, res) {
   const { accessToken, yahooLeagueId } = req.params
   try {
     let result = await yf.getAllUsersLeagues(accessToken, yahooLeagueId)
-    let games = await yf.getGames(accessToken)
     res.json({
       leagues: result,
     })
@@ -107,3 +125,30 @@ async function getLeagueTransactions(req, res) {
   }
 }
 
+async function getRoster(req, res) {
+  const { accessToken, yahooLeagueId, yahooTeamId } = req.params
+  try {
+    let result = await yf.getRoster(accessToken, yahooLeagueId, yahooTeamId)
+    res.json({
+      roster: result.roster,
+    })
+  } catch (error) {
+    debug(error)
+    res.status(500).send('An Error Occured Retrieving Transactions')
+  }
+}
+
+// Need to add to LAMBDAS
+
+async function getGames(req, res) {
+  const { accessToken } = req.params
+  try {
+    let result = await yf.getGameKey(accessToken)
+    res.json({
+      games: result,
+    })
+  } catch (error) {
+    debug(error)
+    res.status(500).send('An Error Occured Retrieving Games')
+  }
+}
--- services/yahooFantasy.js
+++ services/yahooFantasy.js
@@ -2,10 +2,14 @@ var YahooFantasy = require('yahoo-fantasy')
 const debug = require('debug')('app:yahooFantasy')
 const { YAHOO_CLIENT_SECRET, YAHOO_CLIENT_ID } = require('../config')
 const fs = require('fs')
-const GAMEKEY = '399'
+const GAMEKEY = 'nfl'
+const GAMEKEYS = {
+  2020: '399',
+  2019: '390',
+  2018: '380'
+}
 
 function yahooFantasy() {
-
   async function getGames(accessToken) {
     try {
       var yf = new YahooFantasy(YAHOO_CLIENT_ID, YAHOO_CLIENT_SECRET)
@@ -18,12 +22,63 @@ function yahooFantasy() {
     }
   }
 
+  async function getGameKey (accessToken, yahooLeagueId) {
+    console.log('getGameKey')
+       try {
+      var yf = new YahooFantasy(YAHOO_CLIENT_ID, YAHOO_CLIENT_SECRET)
+      yf.setUserToken(accessToken)
+
+      let result = await yf.user.game_leagues('nfl')
+      debug(result)
+      if (result && checkIfLeagueExists(result.games[0].leagues, yahooLeagueId)) {
+        debug('passed')
+        return 'nfl'
+      } else {
+        debug('failed')
+        return '390'
+      }
+
+      return result
+    } catch (error) {
+      debug('ERR', error)
+    }
+  }
+
+  function checkIfLeagueExists (leagues, id) {
+    debug('checkIfLeagueExists')
+    return leagues.find(l => l[0].league_id == '1302')
+  }
+
   async function getMyTeams(accessToken) {
     try {
       var yf = new YahooFantasy(YAHOO_CLIENT_ID, YAHOO_CLIENT_SECRET)
       yf.setUserToken(accessToken)
 
       let result = await yf.user.game_teams(`${GAMEKEY}`)
+      return result
+    } catch (error) {
+      debug('ERR', error)
+    }
+  }
+
+  async function getMyTeam(accessToken, yahooLeagueId) {
+    try {
+      let myTeams = await getMyTeams(accessToken)
+      let myTeam = myTeams.teams[0].teams.find((t) => t.team_key.includes(yahooLeagueId))
+      return await getRoster(accessToken, yahooLeagueId, myTeam.team_id)
+    } catch (error) {
+      debug('ERR', error)
+    }
+  }
+
+  async function getLeagueMeta(accessToken, yahooLeagueId) {
+    console.log('getLeagueMeta')
+    try {
+      var yf = new YahooFantasy(YAHOO_CLIENT_ID, YAHOO_CLIENT_SECRET)
+      yf.setUserToken(accessToken)
+      
+      let result = await yf.league.meta(`${GAMEKEY}.l.${yahooLeagueId}`)
+
       return result
     } catch (error) {
       debug('ERR', error)
@@ -95,14 +150,30 @@ function yahooFantasy() {
     }
   }
 
+  async function getRoster(accessToken, yahooLeagueId, yahooTeamId) {
+    try {
+      var yf = new YahooFantasy(YAHOO_CLIENT_ID, YAHOO_CLIENT_SECRET)
+      yf.setUserToken(accessToken)
+
+      let result = await yf.roster.players(`${GAMEKEY}.l.${yahooLeagueId}.t.${yahooTeamId}`)
+      return result
+    } catch (error) {
+      debug('ERR', error)
+    }
+  }
+
   return {
     getGames,
+    getGameKey,
     getMyTeams,
+    getMyTeam,
+    getLeagueMeta,
     getLeagueSettings,
     getLeagueStandings,
     getLeagueTeams,
     getAllUsersLeagues,
     getLeagueTransactions,
+    getRoster,
   }
 }
 
